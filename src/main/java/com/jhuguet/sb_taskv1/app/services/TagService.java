9f31f0125a8906a14f8c472938af5554af913ee2
Juan Huguet <juan_huguet@epam.com>
2022-09-29 15:05:49 -0500

    DELETE: unnecessary comment

--- src/main/java/com/jhuguet/sb_taskv1/app/services/TagService.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/services/TagService.java
@@ -18,5 +18,4 @@ public interface TagService {
 
     Tag delete(int id) throws InvalidIdInputInformation, IdNotFound, CertificateAssociatedException;
 
-//    Tag addCertificate(int certId) throws BaseException;
 }
