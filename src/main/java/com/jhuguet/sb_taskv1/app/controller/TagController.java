5fae2203ab29a5730b5da39cffaa8584dc8e19f2
Juan Huguet <juan_huguet@epam.com>
2022-09-29 15:06:32 -0500

    ADD: return values in functions and javadoc

--- src/main/java/com/jhuguet/sb_taskv1/app/controller/TagController.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/controller/TagController.java
@@ -33,33 +33,56 @@ public class TagController {
         this.tagService = tagService;
     }
 
+    /**
+     * @param id, if given will return specific Tag that has said ID
+     * @return will return pertaining List-of or single Tag retrieved from DB
+     * @throws IdNotFound                Exception thrown when given ID is not found
+     * @throws InvalidIdInputInformation Exception thrown when given ID is incorrectly entered
+     */
     @ResponseBody
     @GetMapping({"/", "/{id}"})
     public List<Tag> getTags(@PathVariable(required = false) String id) throws IdNotFound, InvalidIdInputInformation {
         if (id != null) {
             List<Tag> tag = new ArrayList<>();
-            tag.add(tagService.get(Integer.valueOf(id)));
+            tag.add(tagService.get(Integer.parseInt(id)));
             return tag;
         }
         return tagService.getAll();
     }
 
+    /**
+     * @param tag Given Tag to save into DB
+     */
     @PostMapping
-    public void saveTag(@RequestBody Tag tag) {
-        tagService.save(tag);
-        logger.info("Saved new tag: " + tag.getName());
+    public Tag saveTag(@RequestBody Tag tag) {
+        logger.info("Saving new tag: " + tag.getName());
+        return tagService.save(tag);
     }
 
+    /**
+     * @param tag Tag to update
+     * @return Updated Tag
+     * @throws IdNotFound                Refer to line 39
+     * @throws InvalidIdInputInformation Refer to line 40
+     */
     @PutMapping
-    public void updateTag(@RequestBody Tag tag) throws IdNotFound, InvalidIdInputInformation {
-        tagService.update(tag);
-        logger.info("Updated tag: " + tag.getId());
+    public Tag updateTag(@RequestBody Tag tag) throws IdNotFound, InvalidIdInputInformation {
+        logger.info("Updating tag: " + tag.getId());
+        return tagService.update(tag);
     }
 
+    /**
+     * @param id ID of Tag to search and drop
+     * @return Deleted Tag
+     * @throws IdNotFound                     Refer to line 39
+     * @throws InvalidIdInputInformation      Refer to line 40
+     * @throws CertificateAssociatedException Exception thrown when Tag is associated to a GiftCertificate and cannot be
+     *                                        deleted
+     */
     @DeleteMapping("/drop/{id}")
-    public void deleteTag(@PathVariable String id) throws IdNotFound, InvalidIdInputInformation, CertificateAssociatedException {
-        tagService.delete(Integer.parseInt(id));
-        logger.info("Dropped tag: " + id);
+    public Tag deleteTag(@PathVariable String id) throws IdNotFound, InvalidIdInputInformation, CertificateAssociatedException {
+        logger.info("Dropping tag: " + id);
+        return tagService.delete(Integer.parseInt(id));
     }
 
 }
