fb1c80a9ced24971717abaa981f850a730022247
Juan Huguet <juan_huguet@epam.com>
2022-09-30 14:56:41 -0500

    ADD: pertaining javadoc

--- src/main/java/com/jhuguet/sb_taskv1/app/services/impl/TagServiceImpl.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/services/impl/TagServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import javax.transaction.TransactionalException;
 import java.util.List;
 
 @Service
@@ -22,21 +21,49 @@ public class TagServiceImpl implements TagService {
         this.tagRepository = tagRepository;
     }
 
-    public List<Tag> getAll() throws RuntimeException {
+
+    /**
+     * Returns all GiftCertificates found in Database
+     *
+     * @return List of GiftCertificates
+     */
+    public List<Tag> getAll() {
         return tagRepository.findAll();
     }
 
+    /**
+     * Returns a single GiftCertificate retrieved based on ID
+     *
+     * @param id Specific Tag ID to search for in Database
+     * @return Will return pertaining Tag retrieved from Database
+     * @throws IdNotFound                Exception thrown when given ID is not found
+     * @throws InvalidIdInputInformation Exception thrown when given ID is incorrectly entered
+     */
     public Tag get(int id) throws IdNotFound, InvalidIdInputInformation {
         validateTag(id);
         return tagRepository.findById(id).get();
     }
 
+    /**
+     * Will save a Tag into Database
+     *
+     * @param tag Tag Object which will be saved
+     * @return Tag object saved into Database
+     */
     @Transactional
-    public Tag save(Tag tag) throws TransactionalException {
+    public Tag save(Tag tag) {
         tagRepository.save(tag);
         return tag;
     }
 
+    /**
+     * Will update whole Tag entity in Database
+     *
+     * @param tag Tag to update
+     * @return Updated Tag
+     * @throws IdNotFound                Exception thrown when given ID is not found
+     * @throws InvalidIdInputInformation Exception thrown when given ID is incorrectly entered
+     */
     @Override
     @Transactional
     public Tag update(Tag tag) throws IdNotFound, InvalidIdInputInformation {
@@ -45,7 +72,16 @@ public class TagServiceImpl implements TagService {
         return tag;
     }
 
-    //Do validation in case there are certificates associated
+    /**
+     * Will delete specific Tag entity in Database
+     *
+     * @param id Specific Tag ID to look for and drop in Database
+     * @return Tag dropped
+     * @throws IdNotFound                     Exception thrown when given ID is not found
+     * @throws InvalidIdInputInformation      Exception thrown when given ID is incorrectly entered
+     * @throws CertificateAssociatedException Exception thrown when Tag is associated to a GiftCertificate and cannot be
+     *                                        deleted
+     */
     @Transactional
     public Tag delete(int id) throws InvalidIdInputInformation, IdNotFound, CertificateAssociatedException {
         Tag tag = get(id);
@@ -56,6 +92,13 @@ public class TagServiceImpl implements TagService {
         return tag;
     }
 
+    /**
+     * Will validate if ID is correctly entered and exists in Database
+     *
+     * @param id Specific ID looked for in Database
+     * @throws IdNotFound                Exception thrown when given ID is not found
+     * @throws InvalidIdInputInformation Exception thrown when given ID is incorrectly entered
+     */
     private void validateTag(int id) throws IdNotFound, InvalidIdInputInformation {
         if (id < 0) {
             throw new InvalidIdInputInformation();
