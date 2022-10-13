f0dfce9b955a0e56235ab73dd7d4356ccb053945
Juan Huguet <juan_huguet@epam.com>
2022-10-13 16:00:17 -0500

    REFACTOR: exception generalized

--- src/main/java/com/jhuguet/sb_taskv1/app/exceptions/InvalidIdInputInformation.java
+++
@@ -1,9 +0,0 @@
-package com.jhuguet.sb_taskv1.app.exceptions;
-
-public class InvalidIdInputInformation extends BaseException {
-
-    public InvalidIdInputInformation() {
-        super("The ID submitted does not comply with database query schema: Negative or null id");
-    }
-
-}
--- src/main/java/com/jhuguet/sb_taskv1/app/handler/GlobalExceptionHandler.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/handler/GlobalExceptionHandler.java
@@ -3,7 +3,7 @@ package com.jhuguet.sb_taskv1.app.handler;
 import com.jhuguet.sb_taskv1.app.exceptions.CertificateAssociatedException;
 import com.jhuguet.sb_taskv1.app.exceptions.ExceptionDetails;
 import com.jhuguet.sb_taskv1.app.exceptions.IdNotFound;
-import com.jhuguet.sb_taskv1.app.exceptions.InvalidIdInputInformation;
+import com.jhuguet.sb_taskv1.app.exceptions.InvalidInputInformation;
 import com.jhuguet.sb_taskv1.app.exceptions.MissingEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +14,10 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(InvalidIdInputInformation.class)
-    public ResponseEntity<?> invalidIdInputInformation(InvalidIdInputInformation exception) {
+    @ExceptionHandler(InvalidInputInformation.class)
+    public ResponseEntity<?> invalidInputInformation(InvalidInputInformation exception) {
         ExceptionDetails details = new ExceptionDetails(exception.getMessage());
-        return new ResponseEntity<>(details, HttpStatus.NOT_ACCEPTABLE);
+        return new ResponseEntity<>(details, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(IdNotFound.class)
@@ -29,7 +29,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(CertificateAssociatedException.class)
     public ResponseEntity<?> certificateAssociated(CertificateAssociatedException exception) {
         ExceptionDetails details = new ExceptionDetails(exception.getMessage());
-        return new ResponseEntity<>(details, HttpStatus.METHOD_NOT_ALLOWED);
+        return new ResponseEntity<>(details, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MissingEntity.class)
