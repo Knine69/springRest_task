f5694f9aed2fe22b7c7924b3bda3d21d4fc7ecd9
Juan Huguet <juan_huguet@epam.com>
2022-10-18 16:44:20 -0500

    ADD: models' repositories

---
+++ src/main/java/com/jhuguet/sb_taskv1/app/repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.jhuguet.sb_taskv1.app.repositories;
+
+import com.jhuguet.sb_taskv1.app.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}
