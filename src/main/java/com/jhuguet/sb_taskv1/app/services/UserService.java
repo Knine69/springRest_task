be34592a1b2c5f2008b71c31442b75cb0a3e3a56
Juan Huguet <juan_huguet@epam.com>
2022-10-26 14:45:22 -0500

    ADD: most used tags related functions declarations and imp

--- src/main/java/com/jhuguet/sb_taskv1/app/services/UserService.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/services/UserService.java
@@ -1,8 +1,11 @@
 package com.jhuguet.sb_taskv1.app.services;
 
 import com.jhuguet.sb_taskv1.app.exceptions.IdNotFound;
+import com.jhuguet.sb_taskv1.app.exceptions.NoExistingOrders;
+import com.jhuguet.sb_taskv1.app.exceptions.NoTagInOrder;
 import com.jhuguet.sb_taskv1.app.exceptions.OrderNotRelated;
 import com.jhuguet.sb_taskv1.app.models.Order;
+import com.jhuguet.sb_taskv1.app.models.Tag;
 import com.jhuguet.sb_taskv1.app.models.User;
 
 import java.util.List;
@@ -12,10 +15,10 @@ public interface UserService {
 
     List<User> getAll();
 
-
     Order getOrder(int userID, int orderID) throws IdNotFound, OrderNotRelated;
 
     List<Order> getOrders(int id) throws IdNotFound;
 
-    Order highestCostOrder(int id) throws IdNotFound;
+    Tag mostUsedTag() throws IdNotFound, NoExistingOrders, NoTagInOrder;
+
 }
--- src/main/java/com/jhuguet/sb_taskv1/app/services/impl/UserServiceImp.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/services/impl/UserServiceImp.java
@@ -1,36 +1,46 @@
 package com.jhuguet.sb_taskv1.app.services.impl;
 
 import com.jhuguet.sb_taskv1.app.exceptions.IdNotFound;
+import com.jhuguet.sb_taskv1.app.exceptions.NoExistingOrders;
+import com.jhuguet.sb_taskv1.app.exceptions.NoTagInOrder;
 import com.jhuguet.sb_taskv1.app.exceptions.OrderNotRelated;
 import com.jhuguet.sb_taskv1.app.models.Order;
+import com.jhuguet.sb_taskv1.app.models.Tag;
 import com.jhuguet.sb_taskv1.app.models.User;
+import com.jhuguet.sb_taskv1.app.repositories.OrderRepository;
+import com.jhuguet.sb_taskv1.app.repositories.TagRepository;
 import com.jhuguet.sb_taskv1.app.repositories.UserRepository;
 import com.jhuguet.sb_taskv1.app.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class UserServiceImp implements UserService {
 
-    private final UserRepository repository;
+    private final UserRepository userRepository;
+    private final TagRepository tagRepository;
+    private final OrderRepository orderRepository;
 
     @Autowired
-    public UserServiceImp(UserRepository repository) {
-        this.repository = repository;
+    public UserServiceImp(UserRepository userRepository, TagRepository tagRepository, OrderRepository orderRepository) {
+        this.userRepository = userRepository;
+        this.tagRepository = tagRepository;
+        this.orderRepository = orderRepository;
     }
 
     @Override
     public User get(int id) throws IdNotFound {
-        return repository.findById(id).orElseThrow(IdNotFound::new);
+        return userRepository.findById(id).orElseThrow(IdNotFound::new);
     }
 
     @Override
     public List<User> getAll() {
-        return repository.findAll();
+        return userRepository.findAll();
     }
 
     @Override
@@ -39,31 +49,52 @@ public class UserServiceImp implements UserService {
         return new ArrayList<>(user.getOrders());
     }
 
-
-    @Override
-    public Order highestCostOrder(int id) throws IdNotFound {
-        Comparator<Order> comparator = Comparator.comparing(Order::getCost);
-
-        getAll().forEach(user -> {
-            User maxCostUser = null;
-            Order maxCostOrder = user.getOrders().stream().max(comparator).get();
-            try {
-                maxCostUser = get(maxCostOrder.getUser().getId());
-            } catch (IdNotFound e) {
-                throw new RuntimeException(e);
-            }
-
-
-        });
-
-        return new Order();
-    }
-
     @Override
     public Order getOrder(int userID, int orderID) throws IdNotFound, OrderNotRelated {
         User user = get(userID);
         return user.getOrders().stream()
                 .filter(x -> x.getId() == orderID).findAny().orElseThrow(OrderNotRelated::new);
+    }
+
+
+    @Override
+    public Tag mostUsedTag() throws IdNotFound, NoExistingOrders, NoTagInOrder {
+        Tag tag = null;
+        Order order = orderRepository.getHighestCostOrder();
+        if (order != null) {
+            Map<Integer, Integer> currentTags = mapCurrentTags();
+
+            order.getCertificates().forEach(c ->
+                    c.getAssociatedTags()
+                            .forEach(t -> currentTags.merge(t.getId(), 1, Integer::sum)));
+
+            int tagID = getMaxTagID(currentTags);
+
+            if (tagID > 0) {
+                tag = tagRepository.findById(tagID).orElseThrow(IdNotFound::new);
+            } else {
+                throw new NoTagInOrder();
+            }
+        } else {
+            throw new NoExistingOrders();
+        }
+
+        return tag;
+    }
+
+    private Integer getMaxTagID(Map<Integer, Integer> tags) {
+        return tags
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .get()
+                .getValue();
+    }
+
+    private Map<Integer, Integer> mapCurrentTags() {
+        Map<Integer, Integer> currentTags = new HashMap<>();
+        tagRepository.findAll().forEach(t -> currentTags.put(t.getId(), 0));
+        return currentTags;
     }
 
 }
