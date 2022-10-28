8294e4b45b5efc0e10ee35017bb4541a8429aa7e
Juan Huguet <juan_huguet@epam.com>
2022-10-28 16:48:28 -0500

    ADD: pre info inserted into db

--- src/main/java/com/jhuguet/sb_taskv1/SbTaskv1Application.java
+++ src/main/java/com/jhuguet/sb_taskv1/SbTaskv1Application.java
@@ -1,7 +1,13 @@
 package com.jhuguet.sb_taskv1;
 
+import com.jhuguet.sb_taskv1.app.BootUpInfo;
+import com.jhuguet.sb_taskv1.app.repositories.GiftCertificateRepository;
+import com.jhuguet.sb_taskv1.app.repositories.OrderRepository;
+import com.jhuguet.sb_taskv1.app.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SbTaskv1Application {
@@ -10,4 +16,11 @@ public class SbTaskv1Application {
         SpringApplication.run(SbTaskv1Application.class, args);
     }
 
+    @Bean
+    public CommandLineRunner demoData(UserRepository userRepo, GiftCertificateRepository certificateRepository, OrderRepository orderRepository) {
+        BootUpInfo bootUpInfo = new BootUpInfo(userRepo, certificateRepository, orderRepository);
+        return args -> {
+            bootUpInfo.prepareInfo();
+        };
+    }
 }
---
+++ src/main/java/com/jhuguet/sb_taskv1/app/BootUpInfo.java
@@ -0,0 +1,79 @@
+package com.jhuguet.sb_taskv1.app;
+
+import com.jhuguet.sb_taskv1.app.models.GiftCertificate;
+import com.jhuguet.sb_taskv1.app.models.Order;
+import com.jhuguet.sb_taskv1.app.models.Tag;
+import com.jhuguet.sb_taskv1.app.models.User;
+import com.jhuguet.sb_taskv1.app.repositories.GiftCertificateRepository;
+import com.jhuguet.sb_taskv1.app.repositories.OrderRepository;
+import com.jhuguet.sb_taskv1.app.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+
+public class BootUpInfo {
+
+    private final UserRepository userRepository;
+    private final GiftCertificateRepository giftCertificateRepository;
+    private final OrderRepository orderRepository;
+
+
+    @Autowired
+    public BootUpInfo(UserRepository userRepository, GiftCertificateRepository giftCertificateRepository, OrderRepository orderRepository) {
+        this.userRepository = userRepository;
+        this.giftCertificateRepository = giftCertificateRepository;
+        this.orderRepository = orderRepository;
+    }
+
+    public void prepareInfo() {
+        for (int i = 0; i < 10000; i++) {
+            if (i < 1001) {
+                addAll(i);
+            } else {
+                addCertificate(i);
+            }
+        }
+    }
+
+    private void addAll(int i) {
+        String localDate = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        GiftCertificate certificate = new GiftCertificate(
+                "Certificate" + i,
+                "Description" + i,
+                BigDecimal.valueOf(10.00),
+                10,
+                localDate,
+                localDate,
+                new HashSet<>()
+        );
+        Tag tag = new Tag("Tag" + i);
+        User user = new User("user" + i, "user" + i + "@domain.com", new HashSet<>());
+        Order order = new Order();
+
+        certificate.assignTag(tag);
+        order.addCertificate(certificate);
+        order.setUser(user);
+
+        giftCertificateRepository.save(certificate);
+        userRepository.save(user);
+        user.placeOrder(order);
+        userRepository.save(user);
+    }
+
+    private void addCertificate(int i) {
+        String localDate = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        GiftCertificate certificate = new GiftCertificate(
+                "Certificate" + i,
+                "Description" + i,
+                BigDecimal.valueOf(10.00),
+                10,
+                localDate,
+                localDate,
+                new HashSet<>()
+        );
+        giftCertificateRepository.save(certificate);
+    }
+}
