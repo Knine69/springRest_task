ba2abaccac3d8e03d8653bdbba98a17c83975288
Juan Huguet <juan_huguet@epam.com>
2022-10-31 12:55:52 -0500

    REFACTOR: message error

--- src/main/java/com/jhuguet/sb_taskv1/app/exceptions/InvalidInputInformation.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/exceptions/InvalidInputInformation.java
@@ -3,7 +3,7 @@ package com.jhuguet.sb_taskv1.app.exceptions;
 public class InvalidInputInformation extends BaseException {
 
     public InvalidInputInformation() {
-        super("The information submitted does not comply with database query schema: Negative or null given.");
+        super("Incorrect input: Please input a positive value.");
     }
 
 }
