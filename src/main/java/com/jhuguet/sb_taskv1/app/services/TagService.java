aaa5686f1064a5eac59bb242c6fe72f1faba2651
Juan Huguet <juan_huguet@epam.com>
2022-11-01 17:20:28 -0500

    ADD: exception throwing and HATEOAS support

--- src/main/java/com/jhuguet/sb_taskv1/app/services/TagService.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/services/TagService.java
@@ -2,6 +2,7 @@ package com.jhuguet.sb_taskv1.app.services;
 
 import com.jhuguet.sb_taskv1.app.exceptions.CertificateAssociatedException;
 import com.jhuguet.sb_taskv1.app.exceptions.IdNotFound;
+import com.jhuguet.sb_taskv1.app.exceptions.InvalidInputInformation;
 import com.jhuguet.sb_taskv1.app.exceptions.MissingEntity;
 import com.jhuguet.sb_taskv1.app.exceptions.PageNotFound;
 import com.jhuguet.sb_taskv1.app.models.Tag;
@@ -13,7 +14,7 @@ public interface TagService {
 
     Tag get(int id) throws IdNotFound;
 
-    Tag save(Tag tag) throws MissingEntity;
+    Tag save(Tag tag) throws MissingEntity, InvalidInputInformation;
 
     Tag delete(int id) throws IdNotFound, CertificateAssociatedException;
 
