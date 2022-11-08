8f07e2979943206a853d78ef5b556a36e3751963
Juan Huguet <juan_huguet@epam.com>
2022-11-08 18:56:40 -0500

    ADD: view controller

--- src/main/java/com/jhuguet/sb_taskv1/app/web/security/ControllerView.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/web/security/ControllerView.java
@@ -1,2 +1,22 @@
-package com.jhuguet.sb_taskv1.app.web.security;public class ControllerView {
+package com.jhuguet.sb_taskv1.app.web.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ControllerView implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry
+                .addViewController("/home")
+                .setViewName("home");
+        registry
+                .addViewController("/")
+                .setViewName("home");
+        registry
+                .addViewController("/login")
+                .setViewName("login");
+    }
+
 }
