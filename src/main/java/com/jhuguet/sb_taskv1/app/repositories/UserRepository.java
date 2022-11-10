3dca596d6e411cafca24fb7d9152baa08ed1443f
Juan Huguet <juan_huguet@epam.com>
2022-11-10 17:24:14 -0500

    ADD: created custom query that filters by username

--- src/main/java/com/jhuguet/sb_taskv1/app/repositories/UserRepository.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/repositories/UserRepository.java
@@ -2,6 +2,10 @@ package com.jhuguet.sb_taskv1.app.repositories;
 
 import com.jhuguet.sb_taskv1.app.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query(value = "SELECT * FROM USERS u WHERE u.username = ?1", nativeQuery = true)
+    User findByUsername(String username);
+
 }
