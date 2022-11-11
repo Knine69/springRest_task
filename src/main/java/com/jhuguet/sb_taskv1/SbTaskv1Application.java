afc913cf1e9974593ae69d8ecb0fd1406ad52cd4
Juan Huguet <juan_huguet@epam.com>
2022-11-11 16:38:45 -0500

    DELETE: unnecessary method invocation

--- src/main/java/com/jhuguet/sb_taskv1/SbTaskv1Application.java
+++ src/main/java/com/jhuguet/sb_taskv1/SbTaskv1Application.java
@@ -3,7 +3,6 @@ package com.jhuguet.sb_taskv1;
 import com.jhuguet.sb_taskv1.app.BootUpInfo;
 import com.jhuguet.sb_taskv1.app.repositories.GiftCertificateRepository;
 import com.jhuguet.sb_taskv1.app.repositories.UserRepository;
-import com.jhuguet.sb_taskv1.app.web.manager.DetailsManager;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,9 +16,8 @@ public class SbTaskv1Application {
     }
 
     @Bean
-    public CommandLineRunner demoData(UserRepository userRepo, GiftCertificateRepository certificateRepository,
-                                      DetailsManager detailsManager) {
-        BootUpInfo bootUpInfo = new BootUpInfo(userRepo, certificateRepository, detailsManager);
+    public CommandLineRunner demoData(UserRepository userRepo, GiftCertificateRepository certificateRepository) {
+        BootUpInfo bootUpInfo = new BootUpInfo(userRepo, certificateRepository);
         return args -> {
             bootUpInfo.prepareInfo();
         };
