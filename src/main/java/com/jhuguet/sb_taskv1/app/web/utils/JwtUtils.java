c7a2d5e0da71a0393e5f102380601071426cb378
Juan Huguet <juan_huguet@epam.com>
2022-11-11 16:39:41 -0500

    ADD: utils class to create JWT

--- src/main/java/com/jhuguet/sb_taskv1/app/web/utils/JwtUtils.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/web/utils/JwtUtils.java
@@ -1,2 +1,31 @@
-package com.jhuguet.sb_taskv1.app.web.utils;public class JwtUtils {
+package com.jhuguet.sb_taskv1.app.web.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtils {
+
+    public String createJwt(String username) throws IOException {
+        Map<String, Object> claims = new HashMap<>();
+        return assembleToken(claims, username);
+    }
+
+    public String assembleToken(Map<String, Object> claims, String username) throws IOException {
+        return Jwts
+                .builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() * 1000 * 60 * 10))
+                .signWith(Keys.hmacShaKeyFor(new FileInputStream("secret-key.pub").readAllBytes()))
+                .compact();
+    }
 }
