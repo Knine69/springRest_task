bb8248088e96f5ee98cda89f3cd6788f0dfc99d0
Juan Huguet <juan_huguet@epam.com>
2022-11-15 17:45:09 -0500

    ADD: new exception cases

--- src/main/java/com/jhuguet/sb_taskv1/app/exceptions/MissingRequiredFields.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/exceptions/MissingRequiredFields.java
@@ -1,2 +1,7 @@
-package com.jhuguet.sb_taskv1.app.exceptions;public class MissingRequiredFields {
+package com.jhuguet.sb_taskv1.app.exceptions;
+
+public class MissingRequiredFields extends BaseException {
+    public MissingRequiredFields() {
+        super("You're missing one or more fields required for inscription. Please revise them.");
+    }
 }
