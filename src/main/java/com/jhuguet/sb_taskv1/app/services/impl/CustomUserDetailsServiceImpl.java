2d8785e4bfc07fdf70eeb09dd5c4052706e4e678
Juan_Huguet <jhuguetospina@gmail.com>
2022-11-17 16:24:16 -0500

    REFACTOR: creation process of UserDetails and addition of utility function

--- src/main/java/com/jhuguet/sb_taskv1/app/services/impl/CustomUserDetailsServiceImpl.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/services/impl/CustomUserDetailsServiceImpl.java
@@ -6,9 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 
 @Service
 public class CustomUserDetailsServiceImpl implements UserDetailsService {
@@ -23,7 +22,15 @@ public class CustomUserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-                new ArrayList<>());
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(new BCryptPasswordEncoder().encode(user.getPassword()))
+                .roles("USER")
+                .build();
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
     }
 }
