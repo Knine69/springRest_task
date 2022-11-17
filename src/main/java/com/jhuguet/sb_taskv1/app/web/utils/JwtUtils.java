cc14e3d63e1742b07c799a90b29759489fc18c6a
Juan_Huguet <jhuguetospina@gmail.com>
2022-11-17 16:22:15 -0500

    ADD: user repository instance, divided logic into methods and returned Object response

--- src/main/java/com/jhuguet/sb_taskv1/app/web/utils/JwtUtils.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/web/utils/JwtUtils.java
@@ -1,7 +1,10 @@
 package com.jhuguet.sb_taskv1.app.web.utils;
 
+import com.jhuguet.sb_taskv1.app.models.User;
+import com.jhuguet.sb_taskv1.app.repositories.UserRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.FileInputStream;
@@ -13,12 +16,31 @@ import java.util.Map;
 @Component
 public class JwtUtils {
 
-    public String createJwt(String username) throws IOException {
+    private UserRepository userRepository;
+
+    @Autowired
+    public JwtUtils(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Map<String, String> createJwt(String username) throws IOException {
         Map<String, Object> claims = new HashMap<>();
         return assembleToken(claims, username);
     }
 
-    public String assembleToken(Map<String, Object> claims, String username) throws IOException {
+    public Map<String, String> assembleToken(Map<String, Object> claims, String username) throws IOException {
+        Map<String, String> jwtResponse = new HashMap<>();
+        User user = userRepository.findByUsername(username);
+        claims.put("id", user.getId());
+
+        String jwt = createToken(claims, username);
+
+        jwtResponse.put("token", jwt);
+        jwtResponse.put("timestamp", new Date().toString());
+        return jwtResponse;
+    }
+
+    private String createToken(Map<String, Object> claims, String username) throws IOException {
         return Jwts
                 .builder()
                 .setClaims(claims)
