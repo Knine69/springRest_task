7b671e952cff5f2c1b4b5371abcf5d2b1363f545
Juan_Huguet <jhuguetospina@gmail.com>
2022-11-17 16:24:31 -0500

    ADD: custom authentication provider

---
+++ src/main/java/com/jhuguet/sb_taskv1/app/web/security/CustomAuthenticationProvider.java
@@ -0,0 +1,44 @@
+package com.jhuguet.sb_taskv1.app.web.security;
+
+import com.jhuguet.sb_taskv1.app.exceptions.WrongCredentials;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @SneakyThrows
+    @Override
+    public Authentication authenticate(Authentication authentication) {
+        String username = authentication.getName();
+        String password = authentication
+                .getCredentials()
+                .toString();
+
+        UserDetails userDetails = giveUserDetails(username);
+
+        if (userDetails != null) {
+            return new UsernamePasswordAuthenticationToken(username, password, userDetails.getAuthorities());
+        } else {
+            throw new WrongCredentials();
+        }
+    }
+
+    UserDetails giveUserDetails(String username) {
+        return User
+                .withUsername(username)
+                .password("*")
+                .roles("USER")
+                .build();
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
