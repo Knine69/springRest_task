0716b7cee9bdf5c4e4631c40843336e9a430d750
Juan_Huguet <jhuguetospina@gmail.com>
2022-11-23 15:07:57 -0500

    REFACTOR: changed expiration date logic and return type response

--- src/main/java/com/jhuguet/sb_taskv1/app/web/utils/JwtUtils.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/web/utils/JwtUtils.java
@@ -1,5 +1,6 @@
 package com.jhuguet.sb_taskv1.app.web.utils;
 
+import com.jhuguet.sb_taskv1.app.models.AuthenticatedAccount;
 import com.jhuguet.sb_taskv1.app.models.User;
 import com.jhuguet.sb_taskv1.app.repositories.UserRepository;
 import io.jsonwebtoken.Jwts;
@@ -23,31 +24,21 @@ public class JwtUtils {
         this.userRepository = userRepository;
     }
 
-    public Map<String, String> createJwt(String username) throws IOException {
+    public AuthenticatedAccount createJwt(String username) throws IOException {
         Map<String, Object> claims = new HashMap<>();
         return assembleToken(claims, username);
     }
 
-    public Map<String, String> assembleToken(Map<String, Object> claims, String username) throws IOException {
+    public AuthenticatedAccount assembleToken(Map<String, Object> claims, String username) throws IOException {
         Map<String, String> jwtResponse = new HashMap<>();
         User user = userRepository.findByUsername(username);
         claims.put("id", user.getId());
-
-        String jwt = createToken(claims, username);
-
-        jwtResponse.put("token", jwt);
-        jwtResponse.put("timestamp", new Date().toString());
-        return jwtResponse;
+        return new AuthenticatedAccount(createToken(claims, username), new Date());
     }
 
     private String createToken(Map<String, Object> claims, String username) throws IOException {
-        return Jwts
-                .builder()
-                .setClaims(claims)
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000l * 60l * 10l))
-                .signWith(Keys.hmacShaKeyFor(new FileInputStream("secret-key.pub").readAllBytes()))
-                .compact();
+        return Jwts.builder().setClaims(claims).setSubject(username).setIssuedAt(new Date()).setExpiration(
+                new Date(System.currentTimeMillis() + 1000l * 60l * 10l)).signWith(
+                Keys.hmacShaKeyFor(new FileInputStream("secret-key.pub").readAllBytes())).compact();
     }
 }
