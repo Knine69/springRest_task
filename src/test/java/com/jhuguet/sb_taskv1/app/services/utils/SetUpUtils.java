69b40f66c5db6cc8ec8058abf9e80a487af2ef06
Juan_Huguet <jhuguetospina@gmail.com>
2022-11-24 16:38:46 -0500

    ADD: creation of several utility functions

--- src/test/java/com/jhuguet/sb_taskv1/app/services/utils/SetUpUtils.java
+++ src/test/java/com/jhuguet/sb_taskv1/app/services/utils/SetUpUtils.java
@@ -1,18 +1,28 @@
 package com.jhuguet.sb_taskv1.app.services.utils;
 
+import com.jhuguet.sb_taskv1.app.models.AuthenticatedAccount;
+import com.jhuguet.sb_taskv1.app.models.AuthenticationRequest;
 import com.jhuguet.sb_taskv1.app.models.GiftCertificate;
 import com.jhuguet.sb_taskv1.app.models.Order;
 import com.jhuguet.sb_taskv1.app.models.Tag;
 import com.jhuguet.sb_taskv1.app.models.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @NoArgsConstructor
@@ -21,36 +31,23 @@ public class SetUpUtils {
         return new Tag("Cloud", new HashSet<>(sampleCertificates()));
     }
 
-    public List<User> sampleUsers() {
-        return List.of(sampleUser(), sampleUser());
+    public Map<String, String> sampleJwt() throws IOException {
+        String token = sampleToken();
+        return Map.of("authorization", "Bearer " + token);
     }
 
-    public User sampleUser() {
-        return new User(0, "user", "user@domain.com", "password", new HashSet<>(sampleOrders()),
-                new HashSet<>(Set.of("USER")));
+    public String sampleToken() throws IOException {
+        return Jwts.builder().setClaims(new HashMap<>(Map.of("id", 1))).setSubject("username").setIssuedAt(new Date())
+                   .setExpiration(new Date(System.currentTimeMillis() + 1000l * 60l * 10l)).signWith(
+                        Keys.hmacShaKeyFor(new FileInputStream("secret-key.pub").readAllBytes())).compact();
     }
 
-    public Set<Order> sampleOrders() {
-        return Set.of(sampleOrder(), sampleOrder());
+    public AuthenticationRequest sampleAuthRequest() {
+        return new AuthenticationRequest("username", "password");
     }
 
-    public Order sampleOrder() {
-        Order order = new Order(1);
-        order.addCertificate(sampleCertificate());
-        return order;
-    }
-
-    public GiftCertificate sampleCertificate() {
-        return GiftCertificate
-                .builder()
-                .name("AWS Certificate")
-                .description("AWS Master's certificate")
-                .price(BigDecimal.valueOf(8.99))
-                .duration(10)
-                .lastUpdateDate("2022-09-20T14:33:15.1301054")
-                .createDate("2022-09-20T14:33:15.1301054")
-                .associatedTags(new HashSet<>(List.of(new Tag("Cloud"))))
-                .build();
+    public AuthenticatedAccount sampleAuthAccount() throws IOException {
+        return new AuthenticatedAccount(sampleToken(), new Date());
     }
 
     public Set<Tag> sampleTags() {
@@ -65,7 +62,40 @@ public class SetUpUtils {
         return List.of(sampleCertificate(), sampleCertificate());
     }
 
+    public GiftCertificate sampleCertificate() {
+        return GiftCertificate.builder().name("AWS Certificate").description("AWS Master's certificate").price(
+                BigDecimal.valueOf(8.99)).duration(10).lastUpdateDate("2022-09-20T14:33:15.1301054").createDate(
+                "2022-09-20T14:33:15.1301054").associatedTags(new HashSet<>(List.of(new Tag("Cloud")))).build();
+    }
+
     public Page<Tag> samplePageTags() {
         return new PageImpl<>(new ArrayList<>(sampleTags()), PageRequest.of(1, 1), sampleCertificates().size());
+    }
+
+    public Page<User> samplePageUsers() {
+        return new PageImpl<>(new ArrayList<>(sampleUsers()), PageRequest.of(1, 1), sampleUsers().size());
+    }
+
+    public List<User> sampleUsers() {
+        return List.of(sampleUser(), sampleUser());
+    }
+
+    public User sampleUser() {
+        return new User(0, "username", "user@domain.com", "password", new HashSet<>(sampleOrders()), "USER");
+    }
+
+    public List<Order> sampleOrders() {
+        return List.of(sampleOrder(), sampleOrder());
+    }
+
+    public Order sampleOrder() {
+        Order order = new Order(1);
+        order.addCertificate(sampleCertificate());
+        return order;
+    }
+
+    public UserDetails sampleUserDetails() {
+        return org.springframework.security.core.userdetails.User.withUsername("username").username("username")
+                                                                 .password("password").roles("USER").build();
     }
 }
