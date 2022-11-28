88eb85c2c9399c9b11d98cad5e867c52f46097b9
Juan_Huguet <jhuguetospina@gmail.com>
2022-11-28 09:55:23 -0500

    REFACTOR: error message

--- src/main/java/com/jhuguet/sb_taskv1/app/exceptions/UsernameNotFound.java
+++ src/main/java/com/jhuguet/sb_taskv1/app/exceptions/UsernameNotFound.java
@@ -2,6 +2,6 @@ package com.jhuguet.sb_taskv1.app.exceptions;
 
 public class UsernameNotFound extends BaseException {
     public UsernameNotFound() {
-        super("Given username is not valid!");
+        super("Given username not found!");
     }
 }
